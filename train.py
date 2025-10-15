f85ea54fc436e23772203b0743d5e999f03386f8
SH0R0UKK <Shoroukelbehairy.09@gmail.com>
2025-10-15 23:52:10 +0300

    added peprocessing stage

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
-# Make predictions on test set
+# Make predictions on test data
 preds = model.predict(X_test)
 
 # Save metrics
